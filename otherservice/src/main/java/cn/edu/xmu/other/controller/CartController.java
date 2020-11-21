5ccde925112f0f8ecbe56fb69776c668f653bbc2
Sagacity2020 <1261396073@qq.com>
2020-11-21 11:01:59 +0800

    second commit

---
+++ otherservice/src/main/java/cn/edu/xmu/other/controller/CartController.java
@@ -0,0 +1,20 @@
+package cn.edu.xmu.other.controller;
+
+import io.swagger.annotations.Api;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@Api(value="购物车服务",tags = "cart")
+@RestController
+@RequestMapping(value = "/cart",produces = "application/json;charset=UTF-8")
+public class CartController {
+
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(CartController.class);
+
+
+
+}
---
+++ otherservice/src/main/java/cn/edu/xmu/other/dao/CartDao.java
@@ -0,0 +1,23 @@
+package cn.edu.xmu.other.dao;
+
+
+
+import cn.edu.xmu.ooad.util.ReturnObject;
+import cn.edu.xmu.other.mapper.ShoppingCartPoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Repository
+public class CartDao {
+
+    @Autowired
+    private ShoppingCartPoMapper shoppingCartPoMapper;
+
+
+
+
+
+}
